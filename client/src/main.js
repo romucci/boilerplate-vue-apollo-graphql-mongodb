cb4e04f42b3e11bc8992542f9b36e6241b3914c9
Lucas <lucasdanielotero1992@gmail.com>
2018-04-22 20:58:25 -0300

    Added Vuex Support

--- client/src/main.js
+++ client/src/main.js
@@ -1,26 +1,13 @@
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
 import Vue from 'vue'
 import VueApollo from 'vue-apollo'
 import Router from './router/router'
 import Vuetify from 'vuetify'
+import { apolloClient } from './apollo/apollo'
 
 import App from './App'
 import store from './store/store'
 
 Vue.config.productionTip = false
-
-const httpLink = new HttpLink({
-  // You should use an absolute URL here
-  uri: '__YOUR__GRAPHQL__URL'
-})
-
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-  connectToDevTools: true
-})
 
 Vue.use(VueApollo)
 Vue.use(Vuetify)
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import apolloClient from '../apollo/apollo'
 
 Vue.use(Vuex)
 
