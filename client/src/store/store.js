df856690ccf76bf5dd776c85c05dc84393cf35cb
Lucas <lucasdanielotero1992@gmail.com>
2018-04-22 21:02:52 -0300

    Added Vuex Support

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import apolloClient from '../apollo/apollo'
+import { apolloClient } from '../apollo/apollo'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
     storeMessage: 'Hello from the Vuex Store!'
+  },
+  actions: {
+    apolloShow () {
+      console.log(apolloClient)
+    }
   }
+
 })
 
 export default store
